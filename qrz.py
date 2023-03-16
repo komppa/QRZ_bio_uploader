dff0f7e4f89f0b11a66ee019370fc87606416c27
komppa <posti@rantakangas.com>
2023-03-16 20:36:31 +0200

    callsign and pw and html from args

--- qrz.py
+++ qrz.py
@@ -1,19 +1,38 @@
 import configparser
 import login
 import bio
-import uuid
+import sys
+import argparse
 
 
 def main():
 
-    config = configparser.ConfigParser()
-    config.read('config.ini')
+    # 1) config.ini
+    # config = configparser.ConfigParser()
+    # config.read('config.ini')
 
-    callsign = config['LOGIN']['callsign']
-    password = config['LOGIN']['password']
+    # callsign = config['LOGIN']['callsign']
+    # password = config['LOGIN']['password']
 
-    if len(password) == 0:
-        exit("You have not set up your callsign and password in config.ini!")
+    # 2) environment variables
+    # callsign = os.environ['CALLSIGN']
+    # password = os.environ['PASSWORD']
+
+    # 3) Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('callsign')
+    parser.add_argument('password')
+    parser.add_argument('html')
+    args = parser.parse_args()
+
+    callsign = args.callsign
+    password = args.password
+    html = args.html
+
+
+    if len(callsign) == 0 or len(password) == 0:
+        # exit("You have not set up your callsign and password in config.ini!")
+        exit("Cannot find callsign and/or password from your environment variables!")
     
     qrz_session_id = login.get_login_token()
     login.validate_callsign(qrz_session_id, callsign)
@@ -23,7 +42,7 @@ def main():
     if not token:
         exit("Could not perform login! Wrong callsign or password?")
 
-    bio.update(callsign, token, '<p>Hi!</p>')
+    bio.update(callsign, token, html)
 
 
 if __name__ == "__main__":
